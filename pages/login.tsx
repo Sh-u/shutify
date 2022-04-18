4386f4f52d0e3c77ac8e7a96c099c4d58290b758
Shu <asunah.flash@gmail.com>
2022-04-18 14:15:40 +0200

    login onClick

--- pages/login.tsx
+++ pages/login.tsx
@@ -12,7 +12,7 @@ import { NextAuthOptions } from "next-auth"
         {Object.values(providers).map(provider => (
          <div key={provider.name}>
           <button className='bg-[#18D860] text-white p-5  rounded-full' 
-          onClick={() => signIn(provider.id, {callbackUrl: "/"})}>Login with {provider.name}</button>
+          onClick={() => signIn()}>Login with {provider.name}</button>
          </div>
         ))}
     </div>
