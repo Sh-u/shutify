0004e3288a8bbf9cf5b78bc9d0f3333b8ff5ee2f
Shu <asunah.flash@gmail.com>
2022-04-18 14:02:34 +0200

    middleware console

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -6,10 +6,16 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server"
 
  export const middleware = async (req) => {
 
-    
+    console.log('middleware here')
 
     const token = await getToken({ req, secret: process.env.JWT_SECRET ?? 'wrong secret middleware',  secureCookie: process.env.NEXTAUTH_URL?.startsWith("https://") ?? !!process.env.VERCEL_URL });
     
+    if (!token){
+        console.log('no token');
+        return NextResponse.error();
+    }
+
+
     const {pathname, origin} = req.nextUrl;
   
     
